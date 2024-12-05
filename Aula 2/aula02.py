3142f919bfd599991580b3b035ce2fc4604c5a51
Isaviiera <jesusisabely9@gmail.com>
2024-12-05 11:23:52 -0300

    Conteúdo aula 2

---
+++ Aula 2/aula02.py
@@ -0,0 +1,6 @@
+print ('Bom dia , good morning, buenos días')
+nome_usuario = "Isabely" # tipo -> str = string
+idade_usuario = 17 # tipo -> int = integer
+altura_usuario = 1.61 # tipo -> float = flutuante
+maior_idade = False # tipo -> bool = boolean
+print("seja bem vindo" , nome_usuario, "sua idade é?" , idade_usuario, " você tem?", altura_usuario, "você é maior de idade?", maior_idade)
